62243e195ba861360430b85e0956e601d1bcc10e
star4z <bdphillips013@gmail.com>
2019-02-12 16:12:07 -0500

    Added class to store inventory item.

---
+++ src/InventoryItem.java
@@ -0,0 +1,11 @@
+public class InventoryItem {
+    public int id = 0;
+    public int barcode = 0;
+    public int qr = 0;
+    public String name = "";
+    public String type = "";
+    public int serial = 0;
+    public String room = "";
+    public String brand = "";
+    public String acquired = "";
+}
